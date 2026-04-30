30501beb5613b8b5af3d692b69b917e3db25b399
Mohammed <m.shehab@zmzm.org>
2026-04-30 16:17:12 +0300

    feat: add production environment configuration and update OAuth redirect URL

--- src/web/src/environments/environment.prod.ts
+++ src/web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
+    production: true,
     supabaseUrl: 'https://zosieclrtzlxgplvfjhs.supabase.co',
     supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpvc2llY2xydHpseGdwbHZmamhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NzQ5NTcsImV4cCI6MjA5MzA1MDk1N30.HAjpxXMobxfgKX1rJmJpgv0i8Xrk9QaKYAxj5tt16FI',
-    oauthRedirectUrl: window.location.origin,
+    oauthRedirectUrl: 'https://shehabnetwork.github.io/qouran_scroller/',
 };
